cb8f69b43ff67e03dfbf8fffbbe0deb203cea3c5
Krishna Srinivas <32317345+Krish6987@users.noreply.github.com>
2020-03-23 15:06:20 +0530

    Update SpringBootAutoProvisionService.java

--- src/main/java/com/wipro/autoui/SpringBootAutoService/AutoprovisionUIService/SpringBootAutoProvisionService.java
+++ src/main/java/com/wipro/autoui/SpringBootAutoService/AutoprovisionUIService/SpringBootAutoProvisionService.java
@@ -42,21 +42,23 @@ public class SpringBootAutoProvisionService {
 		} catch (InterruptedException e) {
 		    e.printStackTrace();
 		}
-		processBuilder.command("bash", "-c", "ansible-playbook /home/ansadmin/"+tool+".yml -i "+ip_address+", -e 'target="+ip_address+"'" );
-		try {
-		    Process process1 = processBuilder.start();
-		    BufferedReader reader1 = new BufferedReader(new InputStreamReader(process1.getInputStream()));
-		    String line1;
-		    while ((line1 = reader1.readLine()) != null) {
-			System.out.println(line1);
-		    }
-		    errorCode = process1.waitFor();
-		    System.out.println("\nExited with error code : " + errorCode);
-		} catch (IOException e) {
-		    e.printStackTrace();
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
+		if(errorCode == 0){
+			processBuilder.command("bash", "-c", "ansible-playbook /home/ansadmin/"+tool+".yml -i "+ip_address+", -e 'target="+ip_address+"'" );
+			try {
+			    Process process1 = processBuilder.start();
+			    BufferedReader reader1 = new BufferedReader(new InputStreamReader(process1.getInputStream()));
+			    String line1;
+			    while ((line1 = reader1.readLine()) != null) {
+				System.out.println(line1);
+			    }
+			    errorCode = process1.waitFor();
+			    System.out.println("\nExited with error code : " + errorCode);
+			} catch (IOException e) {
+			    e.printStackTrace();
+			} catch (InterruptedException e) {
+			    e.printStackTrace();
+			}
 		}
-			return errorCode;
+		return errorCode;
 	}
 }
